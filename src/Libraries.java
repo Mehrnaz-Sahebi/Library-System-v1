9720ca1cf5cda777b97b9ddbe93c092c9259bd2d
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-04-10 21:23:59 +0430

    feat : command add-library

--- src/Libraries.java
+++ src/Libraries.java
@@ -1,12 +1,31 @@
 import java.util.HashSet;
 
 public class Libraries {
-    private HashSet<String> librariesList = new HashSet<String>();
-    public void addLibrary(String libraryId){
-        librariesList.add(libraryId);
+    private HashSet<Library> librariesSet ;
+    public Libraries (){
+        librariesSet = new HashSet<Library>();
     }
-    public static boolean doesLibraryExists(String libraryId ){
-        return librariesList.contains(libraryId);
+
+    public void addLibrary(String libraryId, String name, int yearOfEstablishment, int countOfDesks, String address) {
+        Library library = new Library(libraryId, name, yearOfEstablishment, countOfDesks, address)
+        librariesSet.add(library);
+    }
+
+    public boolean doesLibraryExists(String libraryId) {
+        for (Library library: librariesSet) {
+            if (library.getLibraryId().equals(libraryId)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public Library getLibrary(String libraryId){
+        for (Library library:librariesSet) {
+            if(library.getLibraryId().equals(libraryId)){
+                return library;
+            }
+        }
+        return null;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Libraries libraries = new Libraries();
         Scanner scanner = new Scanner(System.in);
-        String commandLine = scanner.nextLine();
-        String[] partsOfCommandLine = commandLine.split("[#|]+");
-        while(!partsOfCommandLine[0].equals("finish")){
-            if(partsOfCommandLine[0].equals("add-library")){
-
+        String command = scanner.nextLine();
+        String[] partsOfCommand= command.split("[#|]+");
+        while(!partsOfCommand[0].equals("finish")){
+            if(partsOfCommand[0].equals("add-library")){
+                if(libraries.doesLibraryExists(partsOfCommand[1])){
+                    System.out.println("duplicate-id");
+                }
+                else {
+                    libraries.addLibrary(partsOfCommand[1],partsOfCommand[2],Integer.parseInt(partsOfCommand[3]),Integer.parseInt(partsOfCommand[4]),partsOfCommand[5]);
+                    System.out.println("success");
+                }
             }
-            commandLine = scanner.nextLine();
-            partsOfCommandLine = commandLine.split("[#|]+");
+
+            command = scanner.nextLine();
+            partsOfCommand = command.split("[#|]+");
 
         }
 
